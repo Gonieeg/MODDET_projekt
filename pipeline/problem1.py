3928fe0940cb204320d7b9c199ad10af04e089e7
Gonieeg <goniee.g@gmail.com>
2026-02-01 14:32:16 +0100

    problem1 - poprawa funkcji implementującej grzejnik

--- pipeline/problem1.py
+++ pipeline/problem1.py
@@ -5,11 +5,13 @@ from funkcje import * #Kel, Cel, D2, D1B, D1F
 
 ######
 alfa=0.000019
-p=1013
+p=1013*100
 r=287.05
 c=1005
-lambda_wall =  0.17 / 0.25#0.3/0.25
-lambda_window = 0.96 / 0.005 #1.1/0.005  # nie moge mnozyc przez grubosc bo wtedy wieksze i bardziej mrozace jest wall
+lambda_wall =  0.17 / 0.25#0.01#0.17 / 0.25#5#0.3/0.25
+# wall: 0.01 zeby sciany mialy ~~15C gdy zewn=0, 0.05 - wtedy sciany maja 8C
+lambda_window = 0.96 / 0.005#0.96#96#0.96 / 0.005#2#0.005 #1.1/0.005  #1.1# nie moge mnozyc przez grubosc bo wtedy wieksze i bardziej mrozace jest wall
+# window: 0.96 wtedy okno ma ok. 1C
 lambda_air = 0.0262
 P=1267
 
@@ -18,11 +20,11 @@ temp_outside = Kel(0)
 ######
 
 
-ht = 0.1
-T = 10#000 # [s]
+ht = 1
+T = 1000#00 # [s]
 t = int(T/ht)
 
-n = 1
+n = 2
 od = 0
 do = 4
 Nx = 20 * n + 1
@@ -47,7 +49,7 @@ D2x, D2y = D2(Nx), D2(Ny)
 
 
 laplacian = np.kron(id_Ny, D2x) / hx**2 + np.kron(D2y, id_Nx) / hy**2 # sprawdzic w poprzednihc czy taka kolejnosc arguemntow kron
-#alfa = 0.019
+#alfa = 0.0019 # 0.0019 - wciaz jest slaba dyfuzja 0.1 100 ale moze to ok bo maly czas?
 factor = alfa * ht #alfa realistycznie jest mala i prawie sie nie zmienia powietrze (ale przez to rysunek sie praktycznie nie zmienia!!)
 
 A = Id - factor * laplacian
@@ -58,20 +60,21 @@ ind_okno = np.where((Yf == y[-1]) & (Xf >= 1) & (Xf <= 3))[0]
 
 #ideksy ścian pokoju 1 - bez okna
 #ind_wall = np.where((Xf == od) | (Xf == do) | (Yf == od) | (Yf == do) & ~((Xf >= 1) & (Xf <= 3)))[0]
-ind_sciany_N = np.where((Yf == do) & ~((Xf >= 1) & (Xf <= 3)))[0] # bez okna
-ind_sciany_S = np.where((Yf == od))[0]
-ind_sciany_E = np.where((Xf == do))[0]
-ind_sciany_W = np.where((Xf == od))[0]
+ind_sciany_N = np.where((Yf == y[-1]) & ~((Xf >= 1) & (Xf <= 3)))[0] # bez okna
+ind_sciany_S = np.where((Yf == y[0]))[0]
+ind_sciany_E = np.where((Xf == x[-1]))[0]
+ind_sciany_W = np.where((Xf == x[0]))[0]
 # komplet
 ind_scian = np.concatenate([ind_sciany_N, ind_sciany_E, ind_sciany_S, ind_sciany_W])
 
+ind_wnetrza = np.setdiff1d(np.arange(Nx*Ny), np.concatenate([ind_scian, ind_okno]))
 
 # warunki brzegowe
 
 # macierze zadające warunki brzegowe (każda ściana ma swój)(okno ma swój)
 ### kolejnosc kron?
-BsW = np.kron(id_Ny, D1F(Nx)) / hx + lambda_wall/lambda_air * Id
-BsS = np.kron(D1F(Ny), id_Nx) / hy + lambda_wall/lambda_air * Id
+BsW = - (np.kron(id_Ny, D1F(Nx)) / hx) + lambda_wall/lambda_air * Id # tu i nizej minu czy bez?
+BsS = -(np.kron(D1F(Ny), id_Nx) / hy) + lambda_wall/lambda_air * Id # -'''-
 BsE = np.kron(id_Ny, D1B(Nx)) / hx + lambda_wall/lambda_air * Id
 BsN = np.kron(D1B(Ny), id_Nx) / hy + lambda_wall/lambda_air * Id
 BoknoB = np.kron(D1B(Ny), id_Nx) / hy + lambda_window/lambda_air * Id #okno
@@ -88,7 +91,7 @@ A[ind_okno, :] = BoknoB[ind_okno, :]
 # grzejnik
 #odleglosc = 0.2
 # odleglosc nie dziala poniewaz dokladnosc floatow. stawiamy grzjnik w j-tym rzedzie a r polizcymt pozniej
-j = Ny - 2
+j = Ny - 2 #2
 ind_grzejnik = np.where((Yf == y[j]) & (Xf >= 1.5) & (Xf <= 2.5))[0]
 # odleglosci grzejnika od okna w metrach
 rs = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 3.9] #PARZYSTE Z TAKIM HX HY
@@ -107,17 +110,17 @@ def f(grzejnik, u, ust_grzalki):  # albo room zamiast x,y? indeksy pokoju
   u_n = np.zeros_like(u)
   nu = P * r / (p * Area * c)
 
-  if np.mean(u) <= Si:
+  if np.mean(u[ind_wnetrza]) <= Si: #ind_wnetrza dodac do funkcji?
     # tylko tam gdzie grzejnik robic to dzialanie
-    u_n[grzejnik] = u[grzejnik] * nu
+    u_n[grzejnik] = u[grzejnik] * nu#u[grzejnik] * nu
   return u_n
-
+print( P * r / (p * 16 * c))
 #print("temp termostatu graniczna:", S[4])
 
 u_0 = np.full_like(X, Kel(20.0)).flatten()
 u_current = u_0.copy()
 
-u_0 += ht * f(ind_grzejnik, u_0, 4) # zmodyfikowac f zeby bralo koordy grzejnika albo
+#u_0 += ht * f(ind_grzejnik, u_0, 4) # zmodyfikowac f zeby bralo koordy grzejnika albo
   #u_current[ind_grzejnik] += ht * f(x, u_current)
 #print(np.mean(u_0) <= S[4])
 
@@ -132,10 +135,10 @@ u_0[ind_okno] = lambda_window / lambda_air * temp_outside
 #print(lambda_wall / lambda_air * temp_outside)
 #print(lambda_window / lambda_air * temp_outside)
 
-print("mean 0: ", u_current.mean())
+print("mean 0: ", u_current[ind_wnetrza].mean())
 for _ in range(t):
   # równanie du/dt
-  u_current += ht * f(ind_grzejnik, u_current, 4) # zmodyfikowac f zeby bralo koordy grzejnika albo
+  u_current += ht * f(ind_grzejnik, u_current, 4)
   #u_current[ind_grzejnik] += ht * f(x, u_current)
 
   # warunki brzegowe
@@ -144,7 +147,7 @@ for _ in range(t):
 
   # krok symulacji
   u_current = np.linalg.solve(A, u_current)
-  print(f"mean {_+1}: ", u_current.mean())
+  print(f"mean {_+1}: ", u_current[ind_wnetrza].mean())
 
 
 #u_0.mean()
@@ -154,7 +157,7 @@ u_current = Cel(u_current)
 
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 
-levels0 = np.linspace(u_0.min(), u_0.max(), 50)
+levels0 = np.linspace(0,30,50)#u_0.min(), u_0.max(), 50)
 '''
 if Cel(temp_outside) > u_current.min():
   print(u_current < Cel(temp_outside))
@@ -163,7 +166,7 @@ z_min = u_current.min()
 z_max = u_current.max()
 levels = np.linspace(z_min, z_max, 50)
 
-im1 = axs[0].contourf(X, Y, u_0.reshape(Nx, Ny), levels=levels0, cmap='viridis')
+im1 = axs[0].contourf(X, Y, u_0.reshape(Nx, Ny), levels=levels0, cmap='viridis') #? .reshape(Ny, Nx)
 axs[0].set_title('Warunek początkowy')
 fig.colorbar(im1, ax=axs[0], ticks=np.linspace(u_0.min(), u_0.max(), 6))
 
@@ -174,7 +177,16 @@ fig.colorbar(im2, ax=axs[1], ticks=np.linspace(z_min, z_max, 6))
 plt.tight_layout()
 plt.show()
 
+print(u_current)
+cp = plt.contourf(X, Y, u_current.reshape(Nx, Ny), levels=20, vmax=40, cmap='jet')
+
+# Dodanie paska skali
+cb = plt.colorbar(cp)
+cb.set_label('Temperatura')
+
+plt.show()
 # Czemu okno tak mało chłodzi - mimo że ma 1000x skrajniejsze wartości?
+# - i okno i ściany są rachunkowo "zaokrąglone" do dirichleta?
 # Czemu grzejnik grzeje tylko tam gdzie jest i to do milionów?
 # - do milionów bo czas symulacji i wyłącza się gdy jest osiagnięta średnia - a rosnie tylko w tym miejscu
 # - rosnie tylko na grzejniku bo współczynnik dyfuzji jest malutki??
