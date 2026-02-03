808e52b7a5bd9bd58452f26d180259999cad4585
Gonieeg <goniee.g@gmail.com>
2026-02-03 22:02:52 +0100

    problem1 - praca nad klasą

--- pipeline/funkcje.py
+++ pipeline/funkcje.py
@@ -1,59 +1,12 @@
 import numpy as np
-alfa=0.000019
-ro=1013
-r=287.05
-c=1005
-lambda_wall=0.17
-lambda_window=0.96
-lambda_air=0.0262
-P=1267
-
-S = [7, 16, 20, 24, 28]
-temp_outside = 10
-
-# t jako argument?
-# funkcja grzejnika
-
-def room_area(x, y):
-    ## A=b*c tylko jak to wziac po indeksach pokoju
-    return (x[-1]-x[0]) * (y[-1]-y[0])
-
-# kazdy pokoj to osobna macierz
-# wkleje jak zadziala w problem1.py, to jest stare:
-def f(X, Y, u, ust_grzalki, tc): # albo room zamiast x,y? indeksy pokoju
-    A = room_area(X, Y)
-    Si = S[ust_grzalki]
-    if  np.mean(u) <= Si:
-        return u * P * r / (ro * A * c)
-
-
-def przesuniecie_grzejnika(r=0.1): # d - odleglosc od okna in [0.1, 0.2, ..., 3.9]
-    # przy zalozeniu ze hx, hy sa co 0.1?
-    # metr dlugosci
-    # powinien byc 0.1m width
-    # 3.9, 3.8, ...., 0.2, 0.1
-    ind_heater = np.where((Yf == 4 - r) & (Xf >= 1.5) & (Xf <= 2.5))[0]
-    return ind_heater
-
-
-# ściany zewnętrzne
-#def ho(wall, u): # wall - indeksy ściany której temperature liczymy
-#    # u - temperatura na ścianie
-#    return -lambda_wall * 0.25 / lambda_air * (u[wall] - temp_outside) # razy grubość ściany
-
-
-# tylko co jesli to jest sciana dzialowa???
-#def hi(wall, u):
-#    return -lambda_wall * 0.12 / lambda_air * (u[wall] - !!!!!)  # wziąć indeksy po drugiej stronie ściany ??
-
-# okna
-#def g(window, u):
-#    return -lambda_window * 0.005 / lambda_air * (u[window] - temp_outside)  # wziąć indeksy po drugiej stronie ściany ??
-
-
-
-def start_condition(x, y):
-    return np.full_like(x, 20.0)
+alfa = 0.000019
+p = 101300
+r = 287.05
+c = 1005
+lambda_wall = 0.17 #bez podzielenia przez grubosc okno spada do 2 a nie 0
+lambda_window = 0.96 # bez / spada mniej
+lambda_air = 0.0262
+P = 1267
 
 
 def D2(N):
@@ -83,4 +36,22 @@ def Cel(Kel):
   """Przekształca stopnie Kelvina na stopnie Celsjusza"""
   return Kel - 273.15
 
+# ustawienia grzałki ZINENIC
+S = {0: 7, 1: 16, 2: 20, 3: 24, 4: 28} # w *C
+for k in S: # na kelwiny
+    S[k] = Kel(S[k])
+
+
+def f(grzejnik, u, wnetrze, ust_grzalki):  # 1. gdzie dodawac cieplo 2. macierz pokoju 3. przy jakiej sredniej koniec grzania
+    """Funkcja implementująca działanie grzejnika"""
+    Area = 4*4  # zrobic funkcje do tego w kolejnych problemach
+    Si = S[ust_grzalki]
+
+    u_n = np.zeros_like(u)
+    nu = P * r / (p * Area * c)
+
+    if np.mean(u[wnetrze]) <= Si:  # ind_wnetrza dodac do funkcji?
+        # tylko tam gdzie grzejnik robic to dzialanie
+        u_n[grzejnik] = u[grzejnik] * nu
+    return u_n
 
--- pipeline/problem1.py
+++ pipeline/problem1.py
@@ -3,190 +3,159 @@ import matplotlib.pyplot as plt
 from funkcje import * #Kel, Cel, D2, D1B, D1F
 # import all - zmienne tez powinny, ale spr czy cos
 
-######
-alfa=0.000019
-p=1013*100
-r=287.05
-c=1005
-lambda_wall =  0.17 / 0.25#0.01#0.17 / 0.25#5#0.3/0.25
-# wall: 0.01 zeby sciany mialy ~~15C gdy zewn=0, 0.05 - wtedy sciany maja 8C
-lambda_window = 0.96 / 0.005#0.96#96#0.96 / 0.005#2#0.005 #1.1/0.005  #1.1# nie moge mnozyc przez grubosc bo wtedy wieksze i bardziej mrozace jest wall
-# window: 0.96 wtedy okno ma ok. 1C
-lambda_air = 0.0262
-P=1267
+class Problem1:
 
-#S = [7, 16, 20, 24, 28]
-temp_outside = Kel(0)
-######
+  #def __init__(self):
+  # importuj z dane.csv
+    #self.alfa=0.000019
+    #self.p=1013*100
+    #self.r=287.05
+    #self.c=1005
+    #self.lambda_wall =  0.17 / 0.25#0.01#0.17 / 0.25#5#0.3/0.25
+  # wall: 0.01 zeby sciany mialy ~~15C gdy zewn=0, 0.05 - wtedy sciany maja 8C
+    #self.lambda_window = 0.96 / 0.005#0.96#96#0.96 / 0.005#2#0.005 #1.1/0.005  #1.1# nie moge mnozyc przez grubosc bo wtedy wieksze i bardziej mrozace jest wall
+  # window: 0.96 wtedy okno ma ok. 1C
+    #self.lambda_air = 0.0262
+    #self.P=1267
 
+  def symuluj(self, T, ht, n, temp_outside, j, strategia):
 
-ht = 1
-T = 1000#00 # [s]
-t = int(T/ht)
+    #ht = 1
+    #T = 100#00 # [s]
+    t = int(T/ht)
 
-n = 2
-od = 0
-do = 4
-Nx = 20 * n + 1
-Ny = 20 * n + 1
-N = Nx * Ny
+    temp_outside = Kel(temp_outside)
+    #temp_outside = Kel(0)
 
-x = np.linspace(od, do, Nx)
-y = np.linspace(od, do, Ny)
-X, Y = np.meshgrid(x, y)
+    #n = 1
+    od = 0
+    do = 4
+    Nx = 20 * n + 1
+    Ny = 20 * n + 1
+    N = Nx * Ny
 
-hx = x[1] - x[0]
-hy = y[1] - y[0]
-Xf = X.flatten()
-Yf = Y.flatten()
+    x = np.linspace(od, do, Nx)
+    y = np.linspace(od, do, Ny)
+    X, Y = np.meshgrid(x, y)
 
-
-Id = np.eye(Nx*Ny)
-id_Nx, id_Ny = np.eye(Nx), np.eye(Ny)
-
-D2x, D2y = D2(Nx), D2(Ny)
+    hx = x[1] - x[0]
+    hy = y[1] - y[0]
+    Xf = X.flatten()
+    Yf = Y.flatten()
 
 
+    Id = np.eye(N)
+    id_Nx, id_Ny = np.eye(Nx), np.eye(Ny)
 
-laplacian = np.kron(id_Ny, D2x) / hx**2 + np.kron(D2y, id_Nx) / hy**2 # sprawdzic w poprzednihc czy taka kolejnosc arguemntow kron
-#alfa = 0.0019 # 0.0019 - wciaz jest slaba dyfuzja 0.1 100 ale moze to ok bo maly czas?
-factor = alfa * ht #alfa realistycznie jest mala i prawie sie nie zmienia powietrze (ale przez to rysunek sie praktycznie nie zmienia!!)
-
-A = Id - factor * laplacian
+    D2x, D2y = D2(Nx), D2(Ny)
 
 
-# indeksy okna pokoju 1 - od 1 do 3 na top ścianie
-ind_okno = np.where((Yf == y[-1]) & (Xf >= 1) & (Xf <= 3))[0]
+    laplacian = np.kron(id_Ny, D2x) / hx**2 + np.kron(D2y, id_Nx) / hy**2
+    factor = alfa * ht
 
-#ideksy ścian pokoju 1 - bez okna
-#ind_wall = np.where((Xf == od) | (Xf == do) | (Yf == od) | (Yf == do) & ~((Xf >= 1) & (Xf <= 3)))[0]
-ind_sciany_N = np.where((Yf == y[-1]) & ~((Xf >= 1) & (Xf <= 3)))[0] # bez okna
-ind_sciany_S = np.where((Yf == y[0]))[0]
-ind_sciany_E = np.where((Xf == x[-1]))[0]
-ind_sciany_W = np.where((Xf == x[0]))[0]
-# komplet
-ind_scian = np.concatenate([ind_sciany_N, ind_sciany_E, ind_sciany_S, ind_sciany_W])
+    A = Id - factor * laplacian
 
-ind_wnetrza = np.setdiff1d(np.arange(Nx*Ny), np.concatenate([ind_scian, ind_okno]))
+    # indeksy okna pokoju 1 - od 1 do 3 na północnej ścianie
+    ind_okno = np.where((Yf == y[-1]) & (Xf >= 1) & (Xf <= 3))[0]
 
-# warunki brzegowe
+    #ideksy ścian pokoju 1 - bez okna
+    #ind_wall = np.where((Xf == od) | (Xf == do) | (Yf == od) | (Yf == do) & ~((Xf >= 1) & (Xf <= 3)))[0]
+    ind_sciany_N = np.where((Yf == y[-1]) & ~((Xf >= 1) & (Xf <= 3)))[0] # bez okna
+    ind_sciany_S = np.where((Yf == y[0]))[0]
+    ind_sciany_E = np.where((Xf == x[-1]))[0]
+    ind_sciany_W = np.where((Xf == x[0]))[0]
+    # komplet
+    ind_scian = np.concatenate([ind_sciany_N, ind_sciany_E, ind_sciany_S, ind_sciany_W])
 
-# macierze zadające warunki brzegowe (każda ściana ma swój)(okno ma swój)
-### kolejnosc kron?
-BsW = - (np.kron(id_Ny, D1F(Nx)) / hx) + lambda_wall/lambda_air * Id # tu i nizej minu czy bez?
-BsS = -(np.kron(D1F(Ny), id_Nx) / hy) + lambda_wall/lambda_air * Id # -'''-
-BsE = np.kron(id_Ny, D1B(Nx)) / hx + lambda_wall/lambda_air * Id
-BsN = np.kron(D1B(Ny), id_Nx) / hy + lambda_wall/lambda_air * Id
-BoknoB = np.kron(D1B(Ny), id_Nx) / hy + lambda_window/lambda_air * Id #okno
+    # indeksy do obliczania sredniej temperatury pomieszczenia - bez scian (tam gdzie startowo jest 20C)
+    ind_wnetrza = np.setdiff1d(np.arange(Nx*Ny), np.concatenate([ind_scian, ind_okno]))
 
-A[ind_sciany_N, :] = BsN[ind_sciany_N, :]
-A[ind_sciany_E, :] = BsE[ind_sciany_E, :]
-A[ind_sciany_S, :] = BsS[ind_sciany_S, :]
-A[ind_sciany_W, :] = BsW[ind_sciany_W, :]
-A[ind_okno, :] = BoknoB[ind_okno, :]
+    # warunki brzegowe
 
+    # macierze zadające warunki brzegowe (każda ściana ma swój)(i okno ma swój inny)
+    BsW = - (np.kron(id_Ny, D1F(Nx)) / hx) + lambda_wall/lambda_air * Id # tu i nizej minus czy bez?
+    BsS = - (np.kron(D1F(Ny), id_Nx) / hy) + lambda_wall/lambda_air * Id # -'''-
+    BsE = np.kron(id_Ny, D1B(Nx)) / hx + lambda_wall/lambda_air * Id
+    BsN = np.kron(D1B(Ny), id_Nx) / hy + lambda_wall/lambda_air * Id
+    BoknoB = np.kron(D1B(Ny), id_Nx) / hy + lambda_window/lambda_air * Id #okno
 
-
-# przed petla liczaca
-# grzejnik
-#odleglosc = 0.2
-# odleglosc nie dziala poniewaz dokladnosc floatow. stawiamy grzjnik w j-tym rzedzie a r polizcymt pozniej
-j = Ny - 2 #2
-ind_grzejnik = np.where((Yf == y[j]) & (Xf >= 1.5) & (Xf <= 2.5))[0]
-# odleglosci grzejnika od okna w metrach
-rs = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 3.9] #PARZYSTE Z TAKIM HX HY
-
-# ustawienia grzałki ZINENIC
-S = {0: 7, 1: 16, 2: 20, 3: 24, 4: 28} # w *C
-for k in S: # na kelwiny
-    S[k] = Kel(S[k])
-
-# na razie nie jest wprowadzone
-def f(grzejnik, u, ust_grzalki):  # albo room zamiast x,y? indeksy pokoju
-  """Funkcja implementująca działanie grzejnika"""
-  Area = 4*4 # zrobic funkcje do tego w kolejnych problemach
-  Si = S[ust_grzalki]
-
-  u_n = np.zeros_like(u)
-  nu = P * r / (p * Area * c)
-
-  if np.mean(u[ind_wnetrza]) <= Si: #ind_wnetrza dodac do funkcji?
-    # tylko tam gdzie grzejnik robic to dzialanie
-    u_n[grzejnik] = u[grzejnik] * nu#u[grzejnik] * nu
-  return u_n
-print( P * r / (p * 16 * c))
-#print("temp termostatu graniczna:", S[4])
-
-u_0 = np.full_like(X, Kel(20.0)).flatten()
-u_current = u_0.copy()
-
-#u_0 += ht * f(ind_grzejnik, u_0, 4) # zmodyfikowac f zeby bralo koordy grzejnika albo
-  #u_current[ind_grzejnik] += ht * f(x, u_current)
-#print(np.mean(u_0) <= S[4])
-
-#print(f(ind_grzejnik, u_0, 4))#.mean())
-#print(u_0)#.mean())
+    A[ind_sciany_N, :] = BsN[ind_sciany_N, :]
+    A[ind_sciany_E, :] = BsE[ind_sciany_E, :]
+    A[ind_sciany_S, :] = BsS[ind_sciany_S, :]
+    A[ind_sciany_W, :] = BsW[ind_sciany_W, :]
+    A[ind_okno, :] = BoknoB[ind_okno, :]
 
 
-  # warunki brzegowe
-u_0[ind_scian] = lambda_wall / lambda_air * temp_outside
-u_0[ind_okno] = lambda_window / lambda_air * temp_outside
 
-#print(lambda_wall / lambda_air * temp_outside)
-#print(lambda_window / lambda_air * temp_outside)
-
-print("mean 0: ", u_current[ind_wnetrza].mean())
-for _ in range(t):
-  # równanie du/dt
-  u_current += ht * f(ind_grzejnik, u_current, 4)
-  #u_current[ind_grzejnik] += ht * f(x, u_current)
-
-  # warunki brzegowe
-  u_current[ind_scian] = lambda_wall / lambda_air * temp_outside
-  u_current[ind_okno] = lambda_window / lambda_air * temp_outside # to cos sprawia ze okno grzeje? robi skrajnosc
-
-  # krok symulacji
-  u_current = np.linalg.solve(A, u_current)
-  print(f"mean {_+1}: ", u_current[ind_wnetrza].mean())
+    u_0 = np.full_like(X, Kel(20.0)).flatten()
+    u_current = u_0.copy()
 
 
-#u_0.mean()
-#u_current.max()
-u_0 = Cel(u_0)
-u_current = Cel(u_current)
+    # warunki brzegowe
+    u_0[ind_scian] = lambda_wall / lambda_air * temp_outside
+    u_0[ind_okno] = lambda_window / lambda_air * temp_outside
 
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    # przed petla liczaca
+    # grzejnik
+    # odleglosc = 0.2
+    # odleglosc nie dzialala poniewaz dokladnosc floatow. stawiamy grzjnik w j-tym rzedzie a r polizcymt pozniej
+    #j = Ny - 2 #2
+    ind_grzejnik = np.where((Yf == y[j]) & (Xf >= 1.5) & (Xf <= 2.5))[0]
+    # odleglosci grzejnika od okna w metrach rs = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 3.9] #PARZYSTE Z TAKIM HX HY
+    #print(ind_grzejnik)
+    zmianysredniej = []
 
-levels0 = np.linspace(0,30,50)#u_0.min(), u_0.max(), 50)
-'''
-if Cel(temp_outside) > u_current.min():
-  print(u_current < Cel(temp_outside))
-  '''
-z_min = u_current.min()
-z_max = u_current.max()
-levels = np.linspace(z_min, z_max, 50)
+    print("mean 0: ", u_current[ind_wnetrza].mean())
+    for _ in range(t):
+      # równanie du/dt
+      u_current += ht * f(ind_grzejnik, u_current, ind_wnetrza, strategia)
 
-im1 = axs[0].contourf(X, Y, u_0.reshape(Nx, Ny), levels=levels0, cmap='viridis') #? .reshape(Ny, Nx)
-axs[0].set_title('Warunek początkowy')
-fig.colorbar(im1, ax=axs[0], ticks=np.linspace(u_0.min(), u_0.max(), 6))
+      # warunki brzegowe
+      u_current[ind_scian] = lambda_wall / lambda_air * temp_outside
+      u_current[ind_okno] = lambda_window / lambda_air * temp_outside # zimno z okna praktycznie nie robi dyfuzji do wewnątrz
 
-im2 = axs[1].contourf(X, Y, u_current.reshape(Nx, Ny), levels=levels, cmap='viridis')
-axs[1].set_title('Wynik końcowy')
-fig.colorbar(im2, ax=axs[1], ticks=np.linspace(z_min, z_max, 6))
+      # krok symulacji
+      u_current = np.linalg.solve(A, u_current)
+      zmianysredniej.append(u_current[ind_wnetrza].mean())
 
-plt.tight_layout()
-plt.show()
+    print(f"mean {_+1}: ", u_current[ind_wnetrza].mean())
+    print(f"standard deviation:{np.std(u_current[ind_wnetrza])} ")
 
-print(u_current)
-cp = plt.contourf(X, Y, u_current.reshape(Nx, Ny), levels=20, vmax=40, cmap='jet')
 
-# Dodanie paska skali
-cb = plt.colorbar(cp)
-cb.set_label('Temperatura')
+    u_0 = Cel(u_0)
+    u_current = Cel(u_current)
 
-plt.show()
-# Czemu okno tak mało chłodzi - mimo że ma 1000x skrajniejsze wartości?
-# - i okno i ściany są rachunkowo "zaokrąglone" do dirichleta?
-# Czemu grzejnik grzeje tylko tam gdzie jest i to do milionów?
-# - do milionów bo czas symulacji i wyłącza się gdy jest osiagnięta średnia - a rosnie tylko w tym miejscu
-# - rosnie tylko na grzejniku bo współczynnik dyfuzji jest malutki??
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    levels0 = np.linspace(0, 30, 50)
+    levels = np.linspace(u_current.min(), u_current.max(), 50)
+
+    im1 = axs[0, 0].contourf(X, Y, u_0.reshape(Ny, Nx), levels=levels0, cmap='viridis')
+    axs[0, 0].set_title('Warunek początkowy')
+    fig.colorbar(im1, ax=axs[0, 0])
+
+    im2 = axs[0, 1].contourf(X, Y, u_current.reshape(Ny, Nx), levels=levels, cmap='viridis')
+    axs[0, 1].set_title('Wynik końcowy')
+    fig.colorbar(im2, ax=axs[0, 1])
+
+    im3 = axs[1, 0].contourf(
+      X, Y, u_current.reshape(Ny, Nx),
+      levels=20, vmin=0, vmax=40, cmap='jet')
+    axs[1, 0].set_title('Wynik (skala stała)')
+    fig.colorbar(im3, ax=axs[1, 0], label='Temperatura [°C]')
+
+    axs[1, 1].plot(range(t), zmianysredniej)
+    axs[1, 1].set_title('Średnia temperatura w czasie')
+    axs[1, 1].set_xlabel('krok czasu')
+    axs[1, 1].set_ylabel('T [°C]')
+
+    plt.tight_layout()
+    plt.show()
+
+    return u_current
+
+
+sim1 = Problem1()
+a=sim1.symuluj(100, 1, 2, 0, 39, 4)
+print(a)
+
